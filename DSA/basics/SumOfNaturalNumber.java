5baf02bc4e18cfff7b355383649a9a0c6dd164aa
Arun <arun.sdtc@gmail.com>
2024-07-06 13:43:52 +0530

    code till 6 july arun

--- DSA/basics/SumOfNaturalNumber.java
+++ DSA/basics/SumOfNaturalNumber.java
@@ -1,31 +1,37 @@
 package basics;
 
+import java.math.BigInteger;
+
 public class SumOfNaturalNumber {
     public static void main(String[] args) {
-        int start = 3;
-        int end = 5;
-        displayRangeSum(start, end);
-        displaySum(end);
+//        int start = 3;
+        long end = 7876655575575l;
+//        long end = 2000000;
+        BigInteger sum = sumNaturalNoTillEnd(end);
+//        displayRangeSum(start, end);
+
+        System.out.println(sum);
     }
 
-    private static void displaySum(int end) {
-        int sumTillEnd = sumNaturalNoTillEnd(end);
+    private static void displaySum(long end) {
+        BigInteger sumTillEnd = sumNaturalNoTillEnd(end);
         System.out.println("Sum of first n natural no. is " + sumTillEnd);
     }
 
-    public static void displayRangeSum(int start, int end){
-        int sums = sumNaturalNoInRange(start, end);
+    public static void displayRangeSum(long start, long end){
+        BigInteger sums = sumNaturalNoInRange(start, end);
         System.out.println("Sum of " +start+ " to " +end+ " natural no. is " +sums);
     }
 
-    public static int sumNaturalNoTillEnd(int end){
+    public static BigInteger sumNaturalNoTillEnd(long end){
         return sumNaturalNoInRange(1, end);
     }
 
-    public static int sumNaturalNoInRange(int start, int end){
-        int sum = 0;
-        for(int i=start; i<=end; i++){
-            sum = sum+i;
+    public static BigInteger sumNaturalNoInRange(long start, long end){
+        BigInteger sum = BigInteger.valueOf(0);
+        for(long i = start; i <= end; i++){
+            if(i%10000000000l == 0) System.out.println("reached " + i);
+            sum = sum.add(BigInteger.valueOf(i));
         }
         return sum;
     }
--- DSA/basics/SumOfNaturalNumberV2.java
+++ DSA/basics/SumOfNaturalNumberV2.java
@@ -4,17 +4,29 @@ public class SumOfNaturalNumberV2 {
     public static void main(String[] args) {
         int start = 3;
         int end = 5;
-        // displayRangeSum(start, end);
-        // displaySum(end);
+        displayRangeSum(start, end);
+        displaySum(end);
     }
 
-    public static int sumNaturalNoInRangeV2(int start, int end){
-        int sumBefore = sumNaturalNoV2(start - 1);
-        int overallSum = sumNaturalNoV2(end);
-        return overallSum - sumBefore;
+    private static void displaySum(int end) {
+        int sumTillEnd = sumNaturalNoTillEnd(end);
+        System.out.println("Sum of first n natural no. is " + sumTillEnd);
     }
 
-    public static int sumNaturalNoV2(int num){
-        return (num * (num + 1)) / 2;
+    public static void displayRangeSum(int start, int end){
+        int sums = sumNaturalNoInRange(start, end);
+        System.out.println("Sum of " +start+ " to " +end+ " natural no. is " +sums);
+    }
+
+    public static int sumNaturalNoTillEnd(int end){
+        return sumNaturalNoInRange(1, end);
+    }
+
+    public static int sumNaturalNoInRange(int start, int end){
+        int sum = 0;
+        for(int i=start; i<=end; i++){
+            sum = sum+i;
+        }
+        return sum;
     }
 }
